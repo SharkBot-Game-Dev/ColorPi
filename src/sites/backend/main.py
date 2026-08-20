c352bad95b4d6b25224c45478b3700caab5ec6c0
sharkbot-neko <modoggu12345@outlook.com>
2026-08-20 13:56:32 +0900

    feat: フロントエンド

--- src/sites/backend/main.py
+++ src/sites/backend/main.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder="dist/assets",
+    template_folder="dist"
+)
 
-@app.get("/")
-async def index():
-    return render_template()
+@app.route("/")
+def index():
+    return send_from_directory("dist", "index.html")
+
+@app.route("/<path:path>")
+def react_app(path):
+    return send_from_directory("dist", "index.html")
+
+@app.route("/favicon.png")
+def favicon():
+    return send_from_directory("dist", "favicon.png")
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000)
